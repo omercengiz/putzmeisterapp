04bbfda409d4535aad3e479356af1a75b3549c2f
omercengiz <omercengiz607@gmail.com>
2025-07-27 16:40:25 +0300

    Fix: bugfix on field parameters and rule

--- workers/models.py
+++ workers/models.py
@@ -1,10 +1,10 @@
 # models.py
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class BaseWorker(models.Model):
     GROUP_CHOICES = [
-        ('', 'Please select a group'),
         ('PTR', 'PTR'),
         ('SNY', 'SNY'),
     ]
@@ -71,7 +71,7 @@ class BaseWorker(models.Model):
         ('SNY Spare Parts', 'SNY Spare Parts'),
         ('T.Coordination', 'T.Coordination'),
         ('Test', 'Test'),
-        ('Used Machine', 'Used Machine'),
+        ('Used Machine', 'Used Machine'),   
         ('W.Robot', 'W.Robot'),
         ('Warehouse', 'Warehouse'),
         ('Welding', 'Welding'),
@@ -116,20 +116,121 @@ class BaseWorker(models.Model):
         ('Sales Adana', 'Sales Adana')        
     ]
 
+    COST_CENTER_CHOICES = [
+        ("30000", "GENEL MÜDÜRLÜK"),
+        ("30001", "GENEL MÜDÜRLÜK / SATIŞ EXC."),
+        ("30100", "FİNANS DİREKTÖRLÜĞÜ"),
+        ("30120", "FİNANS / MUHASEBE"),
+        ("30140", "BÜTÇE PLANLAMA VE KONTROL"),
+        ("30200", "BİLGİ İŞLEM"),
+        ("30420", "BİNA İNŞAAT"),
+        ("30490", "BAKIM"),
+        ("30500", "SATINALMA"),
+        ("30510", "LOJİSTİK"),
+        ("30520", "DEPO"),
+        ("30550", "MALZEME PLANLAMA"),
+        ("30560", "KALİTE YÖNETİMİ"),
+        ("30600", "KAYNAK OKULU"),
+        ("30700", "İNSAN KAYNAKLARI VE İDARİ İŞLER"),
+        ("30830", "ÇIRAK / STAJYER"),
+        ("31000", "PAZARLAMA"),
+        ("31020", "PAZARLAMA BP"),
+        ("31030", "PAZARLAMA EKS"),
+        ("31100", "YEDEK PARÇA"),
+        ("31250", "SANY YEDEK PARÇA"),
+        ("31300", "SATIŞ DESTEK"),
+        ("31320", "İSTANBUL SATIŞ BP"),
+        ("31450", "SATIŞ - İHRACAT"),
+        ("35000", "TEKNİK DİREKTÖRLÜK"),
+        ("35040", "TEKNİK KOORDİNASYON"),
+        ("35200", "MÜHENDİSLİK"),
+        ("35300", "METOD ŞEFLİĞİ"),
+        ("36010", "ÜRETİM PLANLAMA VE KONTROL"),
+        ("36020", "MEKANİK ATÖLYE"),
+        ("36030", "KAYNAK MONTAJI"),
+        ("36040", "KAYNAK"),
+        ("36041", "KAYNAK ROBOTU 1 (REIS-1)"),
+        ("36042", "KAYNAK ROBOTU 2 (IGM)"),
+        ("36043", "KAYNAK ROBOTU 3 (REIS-2)"),
+        ("36044", "KAYNAK ROBOTU 4 (REIS-4)"),
+        ("36045", "KAYNAK ROBOTU 5"),
+        ("36046", "KAYNAK ROBOTU 6"),
+        ("36047", "KAYNAK ROBOTU 7"),
+        ("36048", "KAYNAK ROBOTU 8"),
+        ("36050", "İŞLEME MERKEZİ 1"),
+        ("36051", "İŞLEME MERKEZİ 2"),
+        ("36052", "İŞLEME MERKEZİ 3"),
+        ("36053", "İŞLEME MERKEZİ 4"),
+        ("36054", "İŞLEME MERKEZİ 5"),
+        ("36060", "BOYA HAZIRLIK"),
+        ("36061", "BOYAMA – ASTARLAMA"),
+        ("36080", "TEST BÖLÜMÜ"),
+        ("36090", "KAZAN HİDROLİK MONTAJ"),
+        ("36091", "ÖN MONTAJ"),
+        ("36092", "KOL MONTAJI"),
+        ("36093", "ANA MONTAJ"),
+        ("36094", "SON MONTAJ"),
+        ("36095", "BSA MONTAJ"),
+        ("36096", "BORU BÜKME"),
+        ("36097", "DOCH ve PUMİ MONTAJ"),
+        ("36098", "ELEKTRİK – MONTAJ"),
+        ("36099", "OPTİMİZASYON"),
+        ("36120", "BETON BORU KAYNAĞI"),
+        ("38110", "SERVİS HADIMKÖY"),
+        ("38112", "SANY SERVİS HADIMKÖY"),
+        ("38120", "SERVİS İZMİR"),
+        ("38121", "SANY SERVİS İZMİR"),
+        ("38122", "SANY SERVİS ANKARA"),
+        ("38130", "SERVİS ANKARA"),
+        ("38132", "SANY SERVİS ANKARA"),
+        ("38140", "SERVİS ESKİŞEHİR"),
+        ("38142", "SANY SERVİS ESKİŞEHİR"),
+        ("38150", "TEKNİK DESTEK PM"),
+        ("38151", "TEKNİK DESTEK SANY"),
+        ("38200", "İZMİR SATIŞ BP"),
+        ("38520", "EXCAVATOR SATIŞ-Hadımköy"),
+        ("38521", "EXCAVATOR SATIŞ-Ataşehir"),
+        ("38522", "EXCAVATOR SATIŞ-İzmir"),
+        ("38523", "EXCAVATOR SATIŞ-Ankara"),
+        ("38540", "EXCAVATOR SATIŞ-Adana"),
+        ("39000", "ANKARA SATIŞ BP"),
+        ("39001", "PMA WORKER"),
+        ("39002", "PCP WORKER"),
+        ("39003", "PMH WORKER"),
+        ("39004", "PEG WORKER"),
+        ("39005", "SANY YENİLENEBİLİR ENERJİ"),
+        ("39010", "PLANNING PCP"),
+        ("39011", "QUALITY PCP"),
+        ("39012", "SALES DISTRIBUTION"),
+        ("39013", "CONTROLLING PCP"),
+        ("39014", "ENGINEERING PCP"),
+        ("39015", "PRE SALES EXCELLENCE SAB"),
+        ("39020", "PURCHASING PMH"),
+        ("39021", "AUDITING PMH"),
+        ("39022", "QUALITY PMH"),
+        ("39023", "ENGINEERING PMH"),
+        ("39024", "SERVICE OPERATION PMH"),
+        ("39030", "ENGINEERING PMA")
+    ]
+
+
     author = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=1)
-    group = models.CharField(max_length=50, choices=GROUP_CHOICES, blank=True, null=True)
-    sicil_no = models.CharField(max_length=50)
-    s_no = models.IntegerField()
-    department_short_name = models.CharField(max_length=100, choices=DIRECTOR_NAME, default='T')
-    department = models.CharField(max_length=100, choices=DEPARTMENT_CLASS, default='Quality')
-    short_class = models.CharField(max_length=50, choices=SHORT_CLASS_CHOICES, default='B')
+    group = models.CharField(max_length=50, choices=GROUP_CHOICES, verbose_name="Group")
+    sicil_no = models.CharField(max_length=50, verbose_name="Sicil No", unique=True)
+    s_no = models.CharField(max_length=50, choices=COST_CENTER_CHOICES, verbose_name="Cost Center")
+    department_short_name = models.CharField(max_length=100, choices=DIRECTOR_NAME, verbose_name="Directorships")
+    department = models.CharField(max_length=100, choices=DEPARTMENT_CLASS, verbose_name="Department")
+    short_class = models.CharField(max_length=50, choices=SHORT_CLASS_CHOICES, verbose_name="Status")
     name_surname = models.CharField(max_length=100)
     date_of_recruitment = models.DateTimeField()
-    work_class = models.CharField(max_length=50, choices=WORK_CLASS_CHOICES, default='Quality')
-    class_name = models.CharField(max_length=50, choices=CLASS_CHOICES, default='İşçi')
+    work_class = models.CharField(max_length=50, choices=WORK_CLASS_CHOICES)
+    class_name = models.CharField(max_length=50, choices=CLASS_CHOICES)
     gross_payment = models.DecimalField(max_digits=15, decimal_places=2)
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default='TRY')
-    bonus = models.IntegerField(null=True, blank=True)
+    bonus = models.IntegerField(validators=[
+        MinValueValidator(0),
+        MaxValueValidator(100)
+    ])
 
     class Meta:
         abstract = True
