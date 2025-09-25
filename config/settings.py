99faf9b39c26008eaf5ce908cf1d7e0c78fa4f03
omercengiz <omercengiz607@gmail.com>
2025-09-26 01:56:40 +0300

    changed the format of the budget values

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+USE_L10N = True
+USE_THOUSAND_SEPARATOR = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
